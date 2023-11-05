96d0ae13e2f663dc23d84efb9791aabc1a2a21cd
shaheen <84270559+shaheen-senpai@users.noreply.github.com>
2023-11-05 05:43:29 +0530

    code module

--- generated_script.py
+++ generated_script.py
@@ -4,14 +4,31 @@ fig,ax = plt.subplots(1,1,figsize=(12,10))
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False) 
 df=pd.read_csv("data.csv")
-df['flag'] = df['flag'].astype('category')
-fig,ax = plt.subplots(1,1,figsize=(12,10))
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-colors = {'F':'red', 'R':'green', 'P':'blue'}
-ax.scatter(df.index, df['flag'], c=df['flag'].map(colors))
-ax.set_xlabel('Index')
-ax.set_ylabel('Flag')
-ax.set_title('Scatter Plot of Flag')
+# Filter the dataframe to only include the 'year' column
+year_df = df['year']
+
+# Group the values by year and count the occurrences
+year_counts = year_df.value_counts()
+
+# Get the labels and sizes for the pie chart
+labels = year_counts.index.tolist()
+sizes = year_counts.values.tolist()
+
+# Create the pie chart
+plt.pie(sizes, labels=labels, startangle=90, autopct='%1.1f%%')
+plt.axis('equal')
+
+# Set the title and labels
+plt.title('Distribution of Years')
+plt.xlabel('Year')
+
+# Set the x and y axes labels
+plt.xlabel('Year')
+plt.ylabel('Percentage')
+
+# Remove the figure suptitle
+fig.suptitle('')
+
+# Show the graph
 my_plot = plt.gcf()
 my_plot.savefig('my_plot1.png')
--- main.py
+++ main.py
@@ -13,11 +13,12 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 figure = "my_plot1"
+code = "generated_script.py"
 plot_area = None
 path = ""
 question = " Ask me a question about the data. " 
 primer1,primer2 = "",""
-
+answer=""
 
 
 
@@ -37,14 +38,11 @@ Graphy_app = """
 |>
 
 <|
-<|{figure}|file_download|label=Download|>
+<|{figure}|file_download|label=Download Graph|><|{code}|file_download|label=Download Code|>
 |>
 
 <|{figure}|image|width="700px"|>
 
-<|
-<|{answer}|>
-|>
 
 
 
@@ -78,7 +76,7 @@ def plot_graph(state) -> None:
     try:
         if os.path.exists('my_plot1.png'):
             os.remove('my_plot1.png')
-            
+
         # Format the question 
         question_to_ask = format_question(primer1, primer2, state.question)   
         print(question_to_ask)
@@ -92,6 +90,7 @@ def plot_graph(state) -> None:
         answer = answer + "\nmy_plot = plt.gcf()\n" + "my_plot.savefig('my_plot1.png')\n"
         
         print(answer,"\n end")
+        
         state.answer = answer
         # plot_area = 
         # plot_area = exec(answer)
@@ -106,7 +105,8 @@ def plot_graph(state) -> None:
         # exec(answer)  
         #sleep the program for 1 second to allow the file to be created
         time.sleep(1)
-        state.figure = "my_plot1.png"   
+        state.figure = "my_plot1.png"       
+        state.code = "generated_script.py"
         state.df = df
     except Exception as e:
         if type(e) == openai.error.APIError:
